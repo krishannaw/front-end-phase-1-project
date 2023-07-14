71dc79874b45bbac359639466f9757e8589bb8bf
Krishanna Williams <krishannaw@gmail.com>
2023-07-14 01:32:53 -0500

    API

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log('connected')
+// script.js
+
+const moviesContainer = document.getElementById('moviesContainer');
+const searchInput = document.getElementById('searchInput');
+
+// Fetch movie data from a public API or your own API (db.json)
+async function fetchMovies() {
+  try {
+    // Replace 'API_URL' with the actual URL of the API you are using
+    const response = await fetch('API_URL');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error fetching movies:', error);
+    return [];
+  }
+}
