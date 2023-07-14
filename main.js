c81ac968f81619036b6f91f8bd92acd4b96c3d51
Krishanna Williams <krishannaw@gmail.com>
2023-07-14 01:40:10 -0500

    API URL

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ const searchInput = document.getElementById('searchInput');
 // Fetch movie data from a public API or your own API (db.json)
 async function fetchMovies() {
   try {
-    // Replace 'API_URL' with the actual URL of the API you are using
-    const response = await fetch('API_URL');
+    const response = await fetch('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup');
     const data = await response.json();
     return data;
   } catch (error) {
